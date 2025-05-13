ef6d0168aeba8027f6e4f433fa71428d9be73526
thelightway <dev@thelightway.kr>
2025-05-14 00:49:16 +0900

    25-05-13-008 [talkpick] 공통 모듈 테스트 - 기술 검토 완료

--- jitpack-study/01/src/main/java/com/likelion/backendplus4/talkpick/common/jpa/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.likelion.backendplus4.talkpick.common.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
